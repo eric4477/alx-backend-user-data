b2a7d18e3781da9d47a742753eb9da3946811296
eric4477 <ericgeorge4477@gmail.com>
2024-08-01 17:35:10 +0300

    add type annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     return re.sub(
         rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
         lambda match: f"{match.group(1)}={redaction}{separator}",
