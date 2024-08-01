2e0a0166fe63af5d70a796b611d7ad8f928b9c5c
eric4477 <ericgeorge4477@gmail.com>
2024-08-01 17:39:01 +0300

    fix format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,12 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     return re.sub(
         rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
         lambda match: f"{match.group(1)}={redaction}{separator}",
-        message
+        message,
     )
