fbe088f40613f52f6fb6bb83a3b526eb8b07f1d0
eric4477 <ericgeorge4477@gmail.com>
2024-08-01 17:57:40 +0300

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
-import re
+import logging
 from typing import List
+from re import sub, escape
 
 
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    return re.sub(
-        rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    pattern = rf"({'|'.join(map(escape, fields))})=.*?{escape(separator)}"
+    return sub(
+        pattern,
         lambda match: f"{match.group(1)}={redaction}{separator}",
-        message,
+        message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR
+        )
