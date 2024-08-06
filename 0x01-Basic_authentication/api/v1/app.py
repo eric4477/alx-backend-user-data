f06c0327cbe5560b2712ac57bdfa2f9a8b56d40e
eric4477 <ericgeorge4477@gmail.com>
2024-08-06 20:09:47 +0300

    format code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,10 +5,8 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 from typing import Literal, Tuple
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,23 +14,25 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ 
+def not_found(error) -> Tuple[str, Literal[404]]:
+    """
     Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> Tuple[str, Literal[401]]:
-    """ 
-    Not found handler
+    """
+    Unauthorized access handler
     """
     return jsonify({"error": "unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> Tuple[str, Literal[403]]:
     """
-     Not found handler
+    Forbidden access handler
     """
     return jsonify({"error": "forbidden"}), 403
 
@@ -40,4 +40,4 @@ def forbidden(error) -> Tuple[str, Literal[403]]:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=int(port))
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,16 +25,20 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """
-    Handles unauthorized access by returning a 401 error.
-    """
-    return abort(401)
 
-@app_views.route('/forbidden/',methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
     """
-    Handles forbidden access by returning a 403 error.
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
     """
-    return abort(403)
+    abort(403)
